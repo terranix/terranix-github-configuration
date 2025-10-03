3d9ffad2a7c22cd5c2e88318d1d1f6a5cbf1eaab
Ingolf Wagner <contact@ingolf-wagner.de>
2025-10-03 23:30:58 +0200

    :closed_lock_with_key: encrypt state with opentofu

---
+++ encryption.tf
@@ -0,0 +1,44 @@
+
+variable "state_encryption" {
+  # Change passphrase to be at least 16 characters long:
+  type      = string
+  sensitive = true
+}
+
+terraform {
+
+  encryption {
+
+    ## Step 1: Add the unencrypted method:
+    #method "unencrypted" "migrate" {}
+
+    ## Step 2: Add the desired key provider:
+    #key_provider "pbkdf2" "my_key_provider_name" {
+    #  passphrase = var.state_encryption
+    #}
+
+    ## Step 3: Add the desired encryption method:
+    #method "aes_gcm" "my_method_name" {
+    #  keys = key_provider.pbkdf2.my_key_provider_name
+    #}
+
+    state {
+      ## Step 4: Link the desired encryption method:
+      method = method.aes_gcm.my_method_name
+
+      ## Step 5: Add the "fallback" block referencing the
+      ## "unencrypted" method.
+      fallback {
+        method = method.unencrypted.migrate
+      }
+
+      ## Step 6: Run "tofu apply".
+
+      ## Step 7: Remove the "fallback" block above and
+      ## consider adding the "enforced" option:
+      # enforced = true
+    }
+
+    ## Step 8: Repeat steps 4-8 for plan{} if needed.
+  }
+}
